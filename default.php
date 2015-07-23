5fc5b1ef267b3bf3cc3bd3140068e0ca30d1e613
Gareth <jengarweb@gmail.com>
2015-07-23 17:26:58 +0100

    first upload

---
+++ default.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+	<link rel="stylesheet" type="text/css" href="css/uikit.almost-flat.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+<head>
+	<title>
+		
+	</title>
+	
+
+</head>
+<body>
+	<div class="uk-container uk-container-center">
+		<div class="uk-grid navheader">
+			<div class="uk-width-5-10">
+				<h1>Bravelilsoldier</h1>
+			</div>
+			<div class="uk-width-5-10">
+				<nav class="uk-navbar uk-margin-large">
+				<ul class="uk-navbar-nav uk-hidden-small">
+                    <li class="uk-active">
+                        <a href="layouts_frontpage.html">Frontpage</a>
+                    </li>
+                    <li>
+                        <a href="layouts_portfolio.html">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="layouts_blog.html">Blog</a>
+                    </li>
+                    <li>
+                        <a href="layouts_documentation.html">Documentation</a>
+                    </li>
+                    <li>
+                        <a href="layouts_contact.html">Contact</a>
+                    </li>
+                </ul>
+				</nav>
+			</div>
+		</div>
+	</div>
+	<div class="banner">
+		...
+	</div> 
+	<div class="uk-container uk-container-center">
+		<div class="uk-grid">
+			<div class="uk-width-1-1 uk-align-center statement">
+				<h1>For those who have helped me, i'll want to help others</h1>
+
+			</div>
+			
+		</div>
+	<hr class="uk-grid-divider">
+	</div>
+
+	<div class="uk-grid uk-grid-margin">
+		
+		<div class="uk-width-1-2">
+			<img width="660" height="400" src="">
+		</div>
+
+		<div class="uk-width-1-2">
+			<p>...</p>
+		</div>
+	</div>
+</body>
+</html>
